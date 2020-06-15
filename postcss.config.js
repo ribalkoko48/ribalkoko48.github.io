5011e2bc7f6c2a9b9ef8ccce7c5d070bc7ebfadc
rybalkoko <rybalkoko@alfastrah.ru>
2020-06-15 17:27:15 +0300

    add auth-page

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({file, options, env}) => ({
-    parser: file.extname === '.sss' ? 'sugarss' : false,
-    plugins: {
-        'postcss-cssnext': {},
-        'cssnano': env === 'production' ? {} : false
-    }
-})
